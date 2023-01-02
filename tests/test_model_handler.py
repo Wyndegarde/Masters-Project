cd3d364f63eaac70f628324d11373e769df32db0
Wyndegarde <76256133+Wyndegarde@users.noreply.github.com>
2023-01-02 11:09:31 +0000

    repository cleaned up

--- tests/test_model_handler.py
+++ tests/test_model_handler.py
@@ -6,7 +6,7 @@ from handlers import ModelHandler
 
 class TestModelHandler:
     def test_spiking_model(self):
-        model_config = ModelHandler()
+        model_config = ModelHandler(model_type="SNN")
         assert model_config.model_type == "SNN"
         assert model_config.spiking_model == True
-        assert model_config.loss_fn == nn.MSELoss()
+        assert isinstance(model_config.loss_fn, nn.MSELoss)
